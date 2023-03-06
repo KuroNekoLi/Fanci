a3058c72ff66b815e5429398a0f92420924bc806
Warren_Lin <warren_lin@cmoney.com.tw>
2023-03-06 16:52:42 +0800

    fix: 6| 建立社團/不公開/新增審核問題，若問題較長或較多會將下方按鈕擠下去

--- app/src/main/java/com/cmoney/kolfanci/ui/screens/group/create/Step2Screen.kt
+++ app/src/main/java/com/cmoney/kolfanci/ui/screens/group/create/Step2Screen.kt
@@ -3,6 +3,8 @@ package com.cmoney.kolfanci.ui.screens.group.create
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
@@ -15,13 +17,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.cmoney.kolfanci.R
 import com.cmoney.kolfanci.ui.common.BlueButton
 import com.cmoney.kolfanci.ui.common.BorderButton
 import com.cmoney.kolfanci.ui.screens.group.setting.group.openness.QuestionItem
 import com.cmoney.kolfanci.ui.theme.Color_29787880
 import com.cmoney.kolfanci.ui.theme.FanciTheme
 import com.cmoney.kolfanci.ui.theme.LocalColor
-import com.cmoney.kolfanci.R
+
 /**
  * 設定權限
  */
@@ -37,103 +40,109 @@ fun Step2Screen(
 ) {
     Spacer(modifier = Modifier.height(20.dp))
     Column {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(LocalColor.current.background)
-                .padding(top = 10.dp, bottom = 10.dp, start = 16.dp, end = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .verticalScroll(rememberScrollState())
+                .weight(1f)
         ) {
-            Image(
-                modifier = Modifier.size(16.dp),
-                painter = painterResource(id = R.drawable.lock),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(color = LocalColor.current.component.other)
-            )
-            Spacer(modifier = Modifier.width(19.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(LocalColor.current.background)
+                    .padding(top = 10.dp, bottom = 10.dp, start = 16.dp, end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.lock),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(color = LocalColor.current.component.other)
+                )
+                Spacer(modifier = Modifier.width(19.dp))
 
-            Text(
-                modifier = Modifier.weight(1f),
-                text = "社團公開度", fontSize = 17.sp, color = LocalColor.current.text.default_100
-            )
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = "社團公開度", fontSize = 17.sp, color = LocalColor.current.text.default_100
+                )
 
-            val publicText = if (isNeedApproval) {
-                "不公開"
-            } else {
-                "公開"
+                val publicText = if (isNeedApproval) {
+                    "不公開"
+                } else {
+                    "公開"
+                }
+
+                Text(
+                    text = publicText,
+                    fontSize = 17.sp,
+                    color = LocalColor.current.specialColor.red
+                )
+
+                Spacer(modifier = Modifier.width(17.dp))
+
+                Switch(
+                    modifier = Modifier.size(51.dp, 31.dp),
+                    checked = isNeedApproval,
+                    onCheckedChange = {
+                        onSwitchApprove.invoke(it)
+                    },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.White,
+                        checkedTrackColor = LocalColor.current.primary,
+                        checkedTrackAlpha = 1f,
+                        uncheckedThumbColor = Color.White,
+                        uncheckedTrackColor = Color_29787880,
+                        uncheckedTrackAlpha = 1f
+                    ),
+                )
             }
 
             Text(
-                text = publicText,
-                fontSize = 17.sp,
-                color = LocalColor.current.specialColor.red
-            )
-
-            Spacer(modifier = Modifier.width(17.dp))
-
-            Switch(
-                modifier = Modifier.size(51.dp, 31.dp),
-                checked = isNeedApproval,
-                onCheckedChange = {
-                    onSwitchApprove.invoke(it)
-                },
-                colors = SwitchDefaults.colors(
-                    checkedThumbColor = Color.White,
-                    checkedTrackColor = LocalColor.current.primary,
-                    checkedTrackAlpha = 1f,
-                    uncheckedThumbColor = Color.White,
-                    uncheckedTrackColor = Color_29787880,
-                    uncheckedTrackAlpha = 1f
-                ),
-            )
-        }
-
-        Text(
-            modifier = Modifier.padding(top = 20.dp, start = 16.dp),
-            text = "*社團無論公開與否，都能被搜尋到*",
-            fontSize = 14.sp,
-            color = LocalColor.current.text.default_50
-        )
-
-        if (isNeedApproval) {
-            Text(
-                modifier = Modifier.padding(top = 25.dp, start = 16.dp),
-                text = "審核問題",
+                modifier = Modifier.padding(top = 20.dp, start = 16.dp),
+                text = "*社團無論公開與否，都能被搜尋到*",
                 fontSize = 14.sp,
                 color = LocalColor.current.text.default_50
             )
 
-            Spacer(modifier = Modifier.height(15.dp))
+            if (isNeedApproval) {
+                Text(
+                    modifier = Modifier.padding(top = 25.dp, start = 16.dp),
+                    text = "審核問題",
+                    fontSize = 14.sp,
+                    color = LocalColor.current.text.default_50
+                )
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(LocalColor.current.background)
-            ) {
-                question.forEach {
-                    QuestionItem(question = it, onClick = {
-                        onEditClick.invoke(it)
-                    })
-                    Spacer(modifier = Modifier.height(1.dp))
-                }
+                Spacer(modifier = Modifier.height(15.dp))
 
-                BorderButton(
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(64.dp)
                         .background(LocalColor.current.background)
-                        .padding(top = 8.dp, bottom = 8.dp, start = 24.dp, end = 24.dp),
-                    text = "新增審核題目",
-                    borderColor = LocalColor.current.text.default_50,
-                    textColor = LocalColor.current.text.default_100
                 ) {
-                    onAddQuestion.invoke()
+                    question.forEach {
+                        QuestionItem(question = it, onClick = {
+                            onEditClick.invoke(it)
+                        })
+                        Spacer(modifier = Modifier.height(1.dp))
+                    }
+
+                    BorderButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(64.dp)
+                            .background(LocalColor.current.background)
+                            .padding(top = 8.dp, bottom = 8.dp, start = 24.dp, end = 24.dp),
+                        text = "新增審核題目",
+                        borderColor = LocalColor.current.text.default_50,
+                        textColor = LocalColor.current.text.default_100
+                    ) {
+                        onAddQuestion.invoke()
+                    }
                 }
             }
+
+
+            Spacer(modifier = Modifier.weight(1f))
         }
-
-
-        Spacer(modifier = Modifier.weight(1f))
 
         Box(
             modifier = Modifier
