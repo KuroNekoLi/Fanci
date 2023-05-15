242afe73060505fb5adae851a5ba81a3ac063698
Warren_Lin <warren_lin@cmoney.com.tw>
2023-05-15 11:04:18 +0800

    feat: 私密頻道 新增 鎖頭顯示

--- app/src/main/java/com/cmoney/kolfanci/ui/screens/chat/message/MessageContentScreen.kt
+++ app/src/main/java/com/cmoney/kolfanci/ui/screens/chat/message/MessageContentScreen.kt
@@ -316,7 +316,6 @@ fun MessageContentScreen(
                                     }
                                 }
                             }
-
                         }
                     }
 
