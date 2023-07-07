f46e67ec51a0c249fb985358adbf081d7fe0f8da
Willie_Chang <willie_chang@cmoney.com.tw>
2023-07-07 14:18:14 +0800

    temp: 改 Page 為 sealed class

--- app/src/main/java/com/cmoney/kolfanci/model/analytics/data/Page.kt
+++ app/src/main/java/com/cmoney/kolfanci/model/analytics/data/Page.kt
@@ -5,7 +5,32 @@ package com.cmoney.kolfanci.model.analytics.data
  *
  * @property eventName 事件名稱
  */
-enum class Page(val eventName: String) {
-    HOME("Home"),
-    MEMBER_PAGE("MemberPage"),
+sealed class Page(val eventName: String) {
+    /**
+     * 首頁社團
+     */
+    object Home: Page("Home")
+
+    /**
+     * 會員頁
+     */
+    object MemberPage: Page("MemberPage")
+
+    object AvatarAndNickname: Page("avatar_and_nickname")
+
+    object NotLoggedInPage: Page("NotLoggedInPage")
+
+    object Avatar: Page("Avatar")
+
+    object Nickname: Page("Nickname")
+
+    object AccountManagement: Page("AccountManagement")
+
+    object Logout: Page("Logout")
+
+    object DeleteAccount: Page("DeleteAccount")
+
+    object TermsOfService: Page("TermsOfService")
+
+    object PrivacyPolicy: Page("PrivacyPolicy")
 }
