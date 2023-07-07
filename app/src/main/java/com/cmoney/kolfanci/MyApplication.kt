bfbe0e152e475e6bff944b272158427b49324817
Willie_Chang <willie_chang@cmoney.com.tw>
2023-07-07 11:44:14 +0800

    temp: 新增引用 UserBehavior, 升級 koin 3.3.3, koin-compose 3.3.0, Page 事件

--- app/src/main/java/com/cmoney/kolfanci/MyApplication.kt
+++ app/src/main/java/com/cmoney/kolfanci/MyApplication.kt
@@ -1,9 +1,12 @@
 package com.cmoney.kolfanci
 
 import android.util.Log
+import com.cmoney.application_user_behavior.AnalyticsAgent
+import com.cmoney.application_user_behavior.di.analyticsModule
 import com.cmoney.backend2.base.di.BACKEND2_SETTING
 import com.cmoney.backend2.base.model.setting.Platform
 import com.cmoney.backend2.base.model.setting.Setting
+import com.cmoney.backend2.di.backendServicesModule
 import com.cmoney.data_logdatarecorder.recorder.LogDataRecorder
 import com.cmoney.kolfanci.di.appModule
 import com.cmoney.kolfanci.di.networkBaseModule
@@ -44,7 +47,9 @@ class MyApplication : XLoginApplication() {
                 viewModule,
                 useCaseModule,
                 appModule,
-                networkBaseModule
+                networkBaseModule,
+                backendServicesModule,
+                analyticsModule
             )
         )
         // 設定Backend2
@@ -52,7 +57,6 @@ class MyApplication : XLoginApplication() {
             appId = resources.getInteger(R.integer.app_id)
             clientId = getString(R.string.app_client_id)
             platform = Platform.Android
-            appVersion = BuildConfig.VERSION_NAME
             domainUrl = BuildConfig.CM_SERVER_URL
         }
 
@@ -73,8 +77,12 @@ class MyApplication : XLoginApplication() {
             backend2Setting.appId = resources.getInteger(R.integer.app_id)
             backend2Setting.clientId = getString(R.string.app_client_id)
             backend2Setting.domainUrl = BuildConfig.CM_SERVER_URL
-            backend2Setting.platform = com.cmoney.backend2.base.model.setting.Platform.Android
+            backend2Setting.platform = Platform.Android
         }
 
+        AnalyticsAgent.initialization(context = this) {
+            platform = com.cmoney.domain_user_behavior.data.information.Platform.Android
+            appId = resources.getInteger(R.integer.app_id)
+        }
     }
 }
---
+++ app/src/main/java/com/cmoney/kolfanci/model/analytics/data/Page.kt
@@ -0,0 +1,11 @@
+package com.cmoney.kolfanci.model.analytics.data
+
+/**
+ * 頁面
+ *
+ * @property eventName 事件名稱
+ */
+enum class Page(val eventName: String) {
+    HOME("Home"),
+    MEMBER_PAGE("MemberPage"),
+}
