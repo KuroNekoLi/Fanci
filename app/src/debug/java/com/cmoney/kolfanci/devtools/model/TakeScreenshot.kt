7a9491e8cb181482cebe39755dd4f29ee64277a1
Willie_Chang <willie_chang@cmoney.com.tw>
2023-07-10 10:53:40 +0800

    refactor: 更新螢幕截圖方法採用 suspend func 避免使用端無法知道截圖完成並儲存好的時間

--- app/src/debug/java/com/cmoney/kolfanci/devtools/model/TakeScreenshot.kt
+++ app/src/debug/java/com/cmoney/kolfanci/devtools/model/TakeScreenshot.kt
@@ -10,12 +10,16 @@ import android.os.Environment
 import android.os.Looper
 import android.view.PixelCopy
 import androidx.core.os.HandlerCompat
+import androidx.core.view.drawToBitmap
 import com.cmoney.tools_android.util.notifyGalleryAddPic
 import com.cmoney.tools_android.util.storeImageAboveQ
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 private const val ROOT_FOLDER_NAME = "fanci_verify_screenshots"
 
@@ -26,35 +30,27 @@ private const val ROOT_FOLDER_NAME = "fanci_verify_screenshots"
  * @param relativePath 儲存的相對路徑 "分類/子分類..."
  * @param name 儲存的原始檔案名稱(會加上時間戳記: xxx-millisTime.jpg)
  */
-fun CoroutineScope.takeScreenshot(context: Context, relativePath: String, name: String) {
+suspend fun takeScreenshot(context: Context, relativePath: String, name: String) {
     val activity = context as? Activity ?: return
     val fileName = "$name-${System.currentTimeMillis()}.jpg"
     try {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            takeScreenshotAboveO(activity = activity) { bitmap ->
-                launch(Dispatchers.IO) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        context.contentResolver.storeImageAboveQ(
-                            folderName = if (relativePath.isNotEmpty()) {
-                                "$ROOT_FOLDER_NAME/$relativePath"
-                            } else {
-                                ROOT_FOLDER_NAME
-                            },
-                            fileName = fileName,
-                            outputBitmap = bitmap
-                        )
-                    } else {
-                        context.storeImageBelowQ(
-                            relativePath = relativePath,
-                            fileName = fileName,
-                            bitmap = bitmap
-                        )
-                    }
-                }
-            }
+        val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            takeScreenshotAboveO(activity = activity)
         } else {
-            val bitmap = takeScreenshotBelowO(activity)
-            launch(Dispatchers.IO) {
+            takeScreenshotBelowO(activity)
+        }
+        withContext(Dispatchers.IO) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                context.contentResolver.storeImageAboveQ(
+                    folderName = if (relativePath.isNotEmpty()) {
+                        "$ROOT_FOLDER_NAME/$relativePath"
+                    } else {
+                        ROOT_FOLDER_NAME
+                    },
+                    fileName = fileName,
+                    outputBitmap = bitmap
+                )
+            } else {
                 context.storeImageBelowQ(
                     relativePath = relativePath,
                     fileName = fileName,
@@ -95,28 +91,34 @@ private fun Context.storeImageBelowQ(
 }
 
 @TargetApi(Build.VERSION_CODES.O)
-private fun takeScreenshotAboveO(activity: Activity, onScreenshot: (Bitmap) -> Unit) {
+private suspend fun takeScreenshotAboveO(activity: Activity): Bitmap {
     val window = activity.window
     val rootView = window.decorView.rootView
     val bitmap = Bitmap.createBitmap(rootView.width, rootView.height, Bitmap.Config.ARGB_8888)
     val locationOfViewInWindow = IntArray(2)
     rootView.getLocationInWindow(locationOfViewInWindow)
-    PixelCopy.request(
-        window,
-        Rect(
-            locationOfViewInWindow[0],
-            locationOfViewInWindow[1],
-            locationOfViewInWindow[0] + rootView.width,
-            locationOfViewInWindow[1] + rootView.height
-        ),
-        bitmap,
-        { copyResult ->
-            if (copyResult == PixelCopy.SUCCESS) {
-                onScreenshot(bitmap)
-            }
-        },
-        HandlerCompat.createAsync(Looper.getMainLooper())
-    )
+    return suspendCancellableCoroutine { continuation ->
+        PixelCopy.request(
+            window,
+            Rect(
+                locationOfViewInWindow[0],
+                locationOfViewInWindow[1],
+                locationOfViewInWindow[0] + rootView.width,
+                locationOfViewInWindow[1] + rootView.height
+            ),
+            bitmap,
+            { copyResult ->
+                if (copyResult == PixelCopy.SUCCESS) {
+                    continuation.resume(bitmap)
+                } else {
+                    continuation.resumeWithException(
+                        IOException("PixelCopy something went wrong. resultCode is $copyResult")
+                    )
+                }
+            },
+            HandlerCompat.createAsync(Looper.getMainLooper())
+        )
+    }
 }
 
 private fun takeScreenshotBelowO(activity: Activity): Bitmap {
@@ -125,5 +127,6 @@ private fun takeScreenshotBelowO(activity: Activity): Bitmap {
     rootView.isDrawingCacheEnabled = true
     val bitmap = Bitmap.createBitmap(rootView.drawingCache)
     rootView.isDrawingCacheEnabled = false
+    rootView.drawToBitmap()
     return bitmap
 }
