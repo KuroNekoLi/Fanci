4c59c1bc42ddeba013777acf36682f165b5cbcdc
Willie_Chang <willie_chang@cmoney.com.tw>
2023-07-26 17:14:52 +0800

    temp: 新增窄版列表元件並調整社團設定設定頁

---
+++ app/src/main/java/com/cmoney/kolfanci/ui/screens/shared/item/WideItem.kt
@@ -0,0 +1,11 @@
+package com.cmoney.kolfanci.ui.screens.shared.item
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WideItem(
+    modifier: Modifier = Modifier
+) {
+
+}
