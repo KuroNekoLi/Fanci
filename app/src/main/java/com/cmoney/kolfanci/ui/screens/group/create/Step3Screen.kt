303a42e5badb6ae7082e188d5ef84dee41d14d09
Warren_Lin <warren_lin@cmoney.com.tw>
2023-07-31 16:40:27 +0800

    fix: 修正 modifier 位置.

--- app/src/main/java/com/cmoney/kolfanci/ui/screens/group/create/Step3Screen.kt
+++ app/src/main/java/com/cmoney/kolfanci/ui/screens/group/create/Step3Screen.kt
@@ -1,9 +1,22 @@
 package com.cmoney.kolfanci.ui.screens.group.create
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -45,9 +58,11 @@ fun Step3Screen(
     }
 
     Spacer(modifier = Modifier.height(20.dp))
-    Column {
+    Column(
+        modifier = modifier
+    ) {
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .verticalScroll(rememberScrollState())
                 .weight(1f)
         ) {
