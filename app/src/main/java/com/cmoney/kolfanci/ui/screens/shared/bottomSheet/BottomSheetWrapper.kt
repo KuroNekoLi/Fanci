9fdac9917ec39a964e72658e1d97c57ec92b2f2d
Warren_Lin <warren_lin@cmoney.com.tw>
2023-10-23 14:07:58 +0800

    fix: remove unused code.

--- app/src/main/java/com/cmoney/kolfanci/ui/screens/shared/bottomSheet/BottomSheetWrapper.kt
+++ app/src/main/java/com/cmoney/kolfanci/ui/screens/shared/bottomSheet/BottomSheetWrapper.kt
@@ -25,44 +25,6 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-//@Preview
-//@Composable
-//@OptIn(ExperimentalMaterialApi::class)
-//fun ModalBottomSheetSample() {
-//    val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
-//    val scope = rememberCoroutineScope()
-//    ModalBottomSheetLayout(
-//        sheetState = state,
-//        sheetContent = {
-//            LazyColumn {
-//                items(50) {
-//                    ListItem(
-//                        text = { Text("Item $it") },
-//                        icon = {
-//                            Icon(
-//                                Icons.Default.Favorite,
-//                                contentDescription = "Localized description"
-//                            )
-//                        }
-//                    )
-//                }
-//            }
-//        }
-//    ) {
-//        Column(
-//            modifier = Modifier.fillMaxSize().padding(16.dp),
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            Text("Rest of the UI")
-//            Spacer(Modifier.height(20.dp))
-//            Button(onClick = { scope.launch { state.show() } }) {
-//                Text("Click to show sheet")
-//            }
-//        }
-//    }
-//}
-
-
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BottomSheetWrapper(
